8254d25b3eb2c88db9794c850b82a67ecfe310cb
Sunita-Gurau <sunitagurau.1999@gmail.com>
2024-09-26 20:00:07 +0545

    fix: active class

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div  :class="isSideNavigationToggled ? 'my-6 mx-2' : 'sm:auto my-6 mx-2 sm:m-6'">
+  <div :class="isSideNavigationToggled ? 'my-6 mx-2' : 'sm:auto my-6 mx-2 sm:m-6'">
     <div class="w-auto  mr-4 cursor-pointer mb-2" :class="isSideNavigationToggled ? 'hidden' : 'hidden sm:block'">
       <div class="flex text-black gap-2 items-center">
         <JbIcon iconName="JoBinsLogo" />
@@ -19,14 +19,23 @@
         :class="isSideNavigationToggled ? 'hidden' : 'hidden sm:block'">
         MAIN MENU</div>
       <ul class="mt-1">
-        <li
-        v-for="menuItem in menuItems" :key="menuItem.id"
-          :class="isSideNavigationToggled ? 'flex items-center justify-center' : 'sm:justify-start flex items-center justify-center '"
-          class="relative hover:bg-gray-2 cursor-pointer px-4 h-12 active rounded-md mb-1">
-          <JbIcon :iconName="menuItem.icon" />
-          <span :class="isSideNavigationToggled ? 'hidden' : 'hidden sm:block'"
-            class="ml-3  text-gray-3 font-semibold tracking-wide hover:text-white transition-colors text-base">
-            {{ menuItem.label }}</span>
+        <li v-for="menuItem in menuItems" :key="menuItem.id" :class="[
+          isSideNavigationToggled ? 'flex items-center justify-center' : 'sm:justify-start flex items-center justify-center',
+          route.path === menuItem.route ? 'bg-gray-2 text-black' : 'text-gray-4'
+        ]" class="relative hover:bg-gray-2  cursor-pointer px-4 h-12 active rounded-md mb-1"
+        @mouseover="setHoveredItem(menuItem, true)"
+        @mouseout="setHoveredItem(menuItem, false)">
+          <router-link :to="menuItem.route" :class="isSideNavigationToggled ? 'flex items-center justify-center' : 'sm:justify-start flex items-center justify-center'">
+            <JbIcon :iconName="menuItem.icon"   :svgFill="
+                     route.path === menuItem.route  || svgFillIcon(menuItem)
+                      ? 'fill-primary'
+                      : 'fill-secondary-6'
+                  "/>
+            <span :class="isSideNavigationToggled ? 'hidden' : 'hidden sm:block'"
+              class="ml-3 font-semibold tracking-wide transition-colors text-base">
+              {{ menuItem.label }}
+            </span>
+          </router-link>
         </li>
       </ul>
     </div>
@@ -35,8 +44,7 @@
         :class="isSideNavigationToggled ? 'hidden' : 'hidden sm:block'">
         PRODUCTS</div>
       <ul class="mt-1">
-        <li
-        v-for="productItem in productItems" :key="productItem.id"
+        <li v-for="productItem in productItems" :key="productItem.id"
           :class="isSideNavigationToggled ? 'flex items-center justify-center' : 'sm:justify-start flex items-center justify-center '"
           class="relative hover:bg-gray-2 cursor-pointer px-4 h-12 active rounded-md mb-1">
           <JbIcon :iconName="productItem.icon" />
@@ -53,12 +61,14 @@
 import JbIcon from './SharedComponents/JbIcon.vue';
 import JbHeader from './SharedComponents/JbHeader.vue';
 import { ref } from "vue"
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
 const isSideNavigationToggled = ref(false)
 const menuItems = ref([
-  { id: 1, label: 'Dashboard', icon: 'SmartHome' },
-  { id: 2, label: 'Order Management', icon: 'ShoppingCart' },
-  { id: 3, label: 'Brand', icon: 'StarBrand' },
+  { id: 1, label: 'Dashboard', icon: 'SmartHome', route: '/dashboard' },
+  { id: 2, label: 'Order Management', icon: 'ShoppingCart', route: '/about' },
+  { id: 3, label: 'Brand', icon: 'StarBrand', route: '/about' },
 ]);
 const productItems = ref([
   { id: 1, label: 'Add Products', icon: 'CirclePlus' },
@@ -77,14 +87,30 @@ const openSideNavigation = () => {
   isSideNavigationToggled.value = false
   emits("toggle-side-navigation", isSideNavigationToggled.value)
 }
+
+interface MenuItem {
+    id: number
+    label: string
+    icon: boolean
+    route: boolean
+  }
+
+const hoveredItem = ref<Set<MenuItem | string>>(new Set())
+
+
+
+const setHoveredItem = (item: MenuItem | string, value: boolean) => {
+  if (value) {
+    hoveredItem.value.add(item)
+  } else {
+    hoveredItem.value.delete(item)
+  }
+}
+
+const isItemHovered = (item: MenuItem) =>
+  hoveredItem.value.has(item)
+
+const svgFillIcon = (item: MenuItem) => {
+  return isItemHovered(item)
+}
 </script>
-<style>
-li.active {
-  background: #F3F4F8;
-
-}
-
-li.active span {
-  color: #23272E;
-}
-</style>
